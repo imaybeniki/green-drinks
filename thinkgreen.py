e993a1321e420444649fb86ed15ac50240a404f4
imaybeniki <nikkimay96@gmail.com>
2018-01-25 12:05:57 -0500

    Added classes to define each type of ingredient

---
+++ thinkgreen.py
@@ -0,0 +1,14 @@
+class Fruit:
+    base = True
+    mixList = set([])
+    amount = 0
+
+class Vegetable:
+    base = False
+    mixList = set([])
+    amount = 0
+
+class Addition:
+    base = True
+    mixList = set([])
+    amount = 0
