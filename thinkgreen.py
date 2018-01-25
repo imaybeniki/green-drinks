88ef70413c87de2ab86f156e7fb9f576db1b0c49
imaybeniki <nikkimay96@gmail.com>
2018-01-25 12:30:42 -0500

    Calculates the total for the list up to each point

--- thinkgreen.py
+++ thinkgreen.py
@@ -1,3 +1,16 @@
+#global variables of each type of fruit/veggie
+global banana
+global apple
+global spinach
+global avocado
+global whey
+global agave
+global doneString
+
+#defining constants
+doneString = "You have reached the maximum ingredients. Press done if complete or continue to add more for additional cost"
+
+#classes to differentiate the types of ingredients
 class Fruit:
     base = True
     mixList = set([])
@@ -12,3 +25,20 @@ class Addition:
     base = True
     mixList = set([])
     amount = 0
+
+#defines flavors as objects
+def defineFlavors():
+    global banana
+    global apple
+    global spinach
+    global avocado
+    global whey
+    global agave
+
+def total(mixList):
+    global doneString
+    total = 0
+    for ingredients in mixList:
+        if total >= 8:
+            print(doneString)
+        total += ingredients.amount
